a64ed3b64ea710924f6fbe1a6e1db50061ecf3b7
qiqingqiren <1239077402@qq.com>
2022-07-21 10:39:54 +0800

    7.21课程

---
+++ leason2_2.java
@@ -0,0 +1,15 @@
+public class leason2_2 {
+    public static void main(String [] args)
+    {
+        int i, sum;
+	    sum = 0;
+           i = 0;
+	    while (i <= 100) 
+        {
+			sum += i;
+			i++;
+	    }
+	    System.out.println("Sum= " + sum);
+	
+    }
+}
