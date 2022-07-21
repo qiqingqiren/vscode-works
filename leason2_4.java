53f76a61bce1ac4d0aeeb473fc2f479bb6b209ed
qiqingqiren <1239077402@qq.com>
2022-07-21 11:32:30 +0800

    git branch -M main git push -u origin main git remote add origin https://github.com/qiqingqiren/vscode-works.git

---
+++ leason2_4.java
@@ -0,0 +1,30 @@
+public class leason2_4 {
+    public static void main(String args[]) {
+		int[] i = new int[3];
+		float[] f = new float[3];
+		boolean[] b = new boolean[3];
+		char[] c = new char[3];
+        
+		for (int j = 0; j < i.length; j++)
+        {
+            System.out.println(i[j]);
+        }
+			
+		for (int j = 0; j < f.length; j++)
+        {
+            System.out.println(f[j]);
+        }
+			
+		for (int j = 0; j < b.length; j++)
+        {
+            System.out.println(b[j]);
+        }
+			
+		for (int j = 0; j < c.length; j++)
+        {
+            System.out.println(c[j]);
+        }
+			
+	}
+    
+}
